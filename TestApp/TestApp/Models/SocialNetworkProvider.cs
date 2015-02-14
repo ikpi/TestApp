2e7748b01797977f67066cf72a8e51bb39438c8e
maksgithub <maxim.tatsenko@gmail.com>
2015-02-15 01:17:15 +0200

    Add SocialNetworkProvider.cs

---
+++ TestApp/TestApp/Models/SocialNetworkProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestApp.Models
+{
+    public class SocialNetworkProvider
+    {
+        private static SocialNetworkProvider instance;
+
+        private readonly ISocialNetwork _sotialNetwork;
+
+        public static SocialNetworkProvider SetInstance(ISocialNetwork sotialNetwork)
+        {
+            instance = new SocialNetworkProvider(sotialNetwork);
+            return instance;
+        }
+
+        public static SocialNetworkProvider Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        private SocialNetworkProvider()
+        { }
+
+        private SocialNetworkProvider(ISocialNetwork sotialNetwork)
+        {
+            _sotialNetwork = sotialNetwork;
+        }
+
+        public ISocialNetwork GetSotialNetwork()
+        {
+            return _sotialNetwork;
+        }
+    }
+}
