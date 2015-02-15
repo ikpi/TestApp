0edc8eed6b67a9220d8fc6f4456d8477971d6bf8
maksgithub <maxim.tatsenko@gmail.com>
2015-02-15 02:17:58 +0200

    Add AllDebits page and view model. Add Debt and Debtor models.

---
+++ TestApp/TestApp/Models/Debt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestApp.Models
+{
+    public class Debt
+    {
+        private Debt(Debtor debtor, int amount, DateTime dateOfCreation, string comment)
+        {
+            Debtor = debtor;
+            Amount = amount;
+            DateOfCreation = dateOfCreation;
+            Comment = comment;
+        }
+
+        public Debt(Debtor debtor, int debt, DateTime dateOfCreation) :
+            this(debtor, debt, dateOfCreation, string.Empty) { }
+
+        public Debtor Debtor { get; private set; }
+
+        public int Amount  { get;private set; }
+
+        public string Comment { get;private set; }
+
+        public DateTime DateOfCreation { get; private set; }
+    }
+}
---
+++ TestApp/TestApp/ViewModels/AllDebtsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GO8.ServerModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestApp.Views;
+
+namespace TestApp.ViewModels
+{
+    public class AllDebtsViewModel : BaseViewModel
+    {
+        AllDebtsPivotPage _page;
+
+        public AllDebtsViewModel(AllDebtsPivotPage page)
+        {
+            _page = page;
+        }
+    }
+}
